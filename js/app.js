c8b636b7b995604859a6f4a0abe0ca9e13165668
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-09-11 18:13:52 +0500

    main

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector(".toggle")
+const elTogglebtn = document.querySelector(".togglebtn")
+elTogglebtn.addEventListener("click",()=>{
+    
+    toggle.classList.toggle("hidden")
+})
